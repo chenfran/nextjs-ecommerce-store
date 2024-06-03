26e79a448fb64ca189ea46bfe068642ba8922a67
Franziska Chen <chenfranziska@gmail.com>
2024-06-03 16:02:05 +0200

    Add thank you page

---
+++ app/thank-you/page.js
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: 'Thank you for your order',
+};
+
+export default function ThankYou() {
+  return (
+    <div>
+      <h1>Thank you for your order.</h1>
+      <div>
+        Your order is confirmed. You will get an email soon. Please check your
+        spam folder.
+      </div>
+    </div>
+  );
+}
