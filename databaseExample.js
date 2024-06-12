0e7b9c2bcd89ee3371e49aeb90ab4ed9abda958f
Franziska Chen <chenfranziska@gmail.com>
2024-06-12 15:59:44 +0200

    Comment out databaseExample.js and databaseExampleQueries.js

--- databaseExample.js
+++ databaseExample.js
@@ -1,18 +1,18 @@
-import { config } from 'dotenv-safe';
-import postgres from 'postgres';
+// import { config } from 'dotenv-safe';
+// import postgres from 'postgres';
 
-console.log(config());
+// console.log(config());
 
-config();
+// config();
 
-const sql = postgres();
+// const sql = postgres();
 
-console.log(
-  await sql`
-SELECT * FROM products
-`,
-);
+// console.log(
+//   await sql`
+// SELECT * FROM products
+// `,
+// );
 
-// We end the connection. This is only for the example.
-// In your code you will want a persistent connection to the database
-await sql.end();
+// // We end the connection. This is only for the example.
+// // In your code you will want a persistent connection to the database
+// await sql.end();
