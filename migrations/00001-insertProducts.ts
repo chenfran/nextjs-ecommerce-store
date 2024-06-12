e4debb108241f7af966aeca2d3483e196eb6a56d
Franziska Chen <chenfranziska@gmail.com>
2024-06-12 15:57:30 +0200

    Create function for the insertProducts.ts file

--- migrations/00001-insertProducts.ts
+++ migrations/00001-insertProducts.ts
@@ -1,7 +1,34 @@
-export async function up(client) {
+import { Sql } from 'postgres';
 
+const products = [
+  { id: 1, name: 'Chicken', price: 10 },
+  { id: 2, name: 'Bonbon', price: 10 },
+  { id: 3, name: 'Panda', price: 10 },
+  { id: 4, name: 'Dino', price: 10 },
+  { id: 5, name: 'Parrot', price: 10 },
+  { id: 6, name: 'Strawberry', price: 10 },
+];
+
+export async function up(sql: Sql) {
+  for (const product of products) {
+    await sql`
+      INSERT INTO
+        products (name, price)
+      VALUES
+        (
+          ${product.name},
+          ${product.price}
+        )
+    `;
+  }
 }
 
-export async function down(client) {
-
+export async function down(sql: Sql) {
+  for (const product of products) {
+    await sql`
+      DELETE FROM products
+      WHERE
+        id = ${product.id}
+    `;
+  }
 }
