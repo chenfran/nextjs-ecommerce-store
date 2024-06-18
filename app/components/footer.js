71bc75873e3e12af7b31fc25689f0230be9a7735
Franziska Chen <chenfranziska@gmail.com>
2024-06-18 15:53:00 +0200

    Change the styling of footer.js

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="footer footer-center p-4 bg-neutral text-neutral-content">
+    <footer className="footer footer-center p-4 bg-gray-700 text-neutral-50">
       <aside>
         <p>Created by Franziska Chen, Vienna 2024</p>
       </aside>
