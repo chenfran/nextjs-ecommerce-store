97c643f8dd5f7a8f7ec4e2a0582912b6afe39685
Franziska Chen <chenfranziska@gmail.com>
2024-06-19 13:26:45 +0200

    Add styling to thank-you page

--- app/thank-you/page.js
+++ app/thank-you/page.js
@@ -7,17 +7,20 @@ export const metadata = {
 export default function ThankYou() {
   return (
     <div>
-      <div>
-        <h1>Thank you for your order!</h1>
-        <br />
-        <p>
-          Your order has been successfully placed. We will send you a
-          confirmation email.
-        </p>
-      </div>
-      <div>
-        <Link href="/">Go to Homepage</Link>
-      </div>
+      <Link href="/">
+        <div>
+          <h1 className="text-5xl font-bold text-center mt-8 mb-12">
+            Thank you for your order!
+          </h1>
+          <div className="text-center items-center">
+            <p>
+              Your order has been successfully placed. We will send you a
+              confirmation email.
+            </p>
+            <button className="btn btn-outline m-8">Go to Homepage</button>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 }
